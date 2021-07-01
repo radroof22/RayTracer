8511d97ff9304e38729f867ff0c8e7191e93b1ee
Rohan <mehta.rohan@outlook.com>
2021-07-01 18:44:35 -0400

    refractor equation

--- main.cpp
+++ main.cpp
@@ -4,23 +4,29 @@
 
 #include <iostream>
 
-bool hit_sphere(const point3& center, double radius, const ray& r){
+double hit_sphere(const point3& center, double radius, const ray& r){
     vec3 oc = r.origin() - center; // t value
 
     // expanding the equation
-    auto a = dot(r.direction() , r.direction());
-    auto b = 2.0 * dot(oc, r.direction());
-    auto c = dot(oc, oc) - radius*radius;
-    auto discriminant = b*b - 4*a*c;
-    return discriminant > 0;
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius* radius;
+    auto discriminant = half_b*half_b - a*c;
+    if (discriminant < 0){
+        return -1.0;
+    }else{
+        return (-half_b - sqrt(discriminant) ) / a;
+    }
 }
 
 color ray_color(const ray& r) {
-    if(hit_sphere(point3(0, 0, -1), .5, r)){
-        return color(1,0, 0); // return red if the light is hitting sphere
+    auto t = hit_sphere(point3(0,0,-1), 0.5, r);
+    if(t > 0.0){
+        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
     }
     vec3 unit_direction = unit_vector(r.direction());
-    auto t = 0.5 * (unit_direction.y() + 1.0);
+    t = 0.5*(unit_direction.y() + 1.0);
     // linear gradient because (1-t) * startValue + t*endValue
     return (1.0-t) * color(1.0,1.0,1.0) + t*color(.5, .7, 1.0);
 }
